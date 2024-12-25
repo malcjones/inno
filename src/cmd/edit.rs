a2f1b102b7e2851cbd391e5c638fd978b0d509de
malcjones <33630760+malcjones@users.noreply.github.com>
2024-12-24 21:12:24 -0600

    fixed some bad practice

--- src/cmd/edit.rs
+++ src/cmd/edit.rs
@@ -6,9 +6,9 @@ pub fn run(dispatch: &mut Dispatch, args: &[String]) -> Result<()> {
         [id, new_name, new_url, new_tags @ ..] => {
             let id = id.parse().context("Invalid ID")?;
             let mut bookmark = dispatch.store_mut().get_mut(id).context("Bookmark not found")?;
-            bookmark.name = new_name.clone();
-            bookmark.url = new_url.clone();
-            bookmark.tags = new_tags.iter().map(|s| s.clone()).collect();
+            bookmark.name.clone_from(new_name);
+            bookmark.url.clone_from(new_url);
+            bookmark.tags = new_tags.iter().cloned().collect();
             println!("Edited bookmark with ID {}", id);
         }
         _ => anyhow::bail!("Invalid arguments"),
